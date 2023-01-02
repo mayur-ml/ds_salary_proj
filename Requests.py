6dc38e4eb45e91cf3c56c4503b930fed85b4ac41
Mayur Balkrishna Ingole <108168115+TrillionaireMayur@users.noreply.github.com>
2023-01-03 00:30:53 +0530

    FLASK

---
+++ Requests.py
@@ -0,0 +1,17 @@
+"""
+Author : Mayur ingole
+Date : 02-01-2023
+Time : 12:26 AM
+Version : 01
+"""
+
+import requests 
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL,headers=headers, json=data) 
+
+r.json()
